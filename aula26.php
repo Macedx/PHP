1b5e74e531135de5a1300aee6162ad1ad55a6062
Ana Macedo <108587155+Macedx@users.noreply.github.com>
2024-03-26 21:40:26 -0300

    Add files via upload

---
+++ aula26.php
@@ -0,0 +1,14 @@
+
+    <?php
+// Escreva um programa que leia 3 números inteiros:
+// - O 1° número corresponde ao 1° termo de uma PA;
+// - O 2° número corresponde à razão da sequência;
+// - O 3° número corresponde ao número de termos. 
+    $num1 = 1;
+    $num2 = 2;
+    $num3 = 5;
+
+    for($i = 0; $i<=$num3; $i++){
+        echo "\n" .$num1 = $num1+$num2;
+    }
+     ?>
